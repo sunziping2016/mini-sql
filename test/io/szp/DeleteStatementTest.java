6a6b6c84552e73206dfd4c1b10b7808ea5ed4764
Liuxiang15 <thss15_liux@163.com>
2019-06-07 18:22:03 +0800

    修改join on的测试

---
+++ test/io/szp/DeleteStatementTest.java
@@ -0,0 +1,45 @@
+package io.szp;
+
+import io.szp.exception.SQLException;
+import io.szp.schema.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DeleteStatementTest {
+    private Global global;
+    private Session session;
+
+    @BeforeEach
+    public void init() throws SQLException {
+        global = new Global(Config.root);
+        session = new Session();
+        global.removeAllDatabases();
+    }
+
+    @Test
+    public void deleteWhere() throws SQLException {
+        Table result;
+        global.execute("create database test; use test", session);
+        global.execute("create table test (i int, s string)", session);
+        global.execute("insert into test values (+-+-1, '11'),  (+-+-2, '22'), (+-+-2, '3'),(null, \"hello\\t world\")", session);
+        result = global.execute("delete from test where s=\"hello\\t world\"", session);
+        assertEquals(new Table("RESULT",
+                new Column[] { new Column("DELETED", Type.INT) },
+                new Object[][] {
+                        new Object[] { 1 }
+                }
+        ), result);
+
+//        删除多条记录
+        result = global.execute("delete from test where i=2", session);
+        assertEquals(new Table("RESULT",
+                new Column[] { new Column("DELETED", Type.INT) },
+                new Object[][] {
+                        new Object[] { 2 }
+                }
+        ), result);
+    }
+}
--- test/io/szp/SelectStatementTest.java
+++ test/io/szp/SelectStatementTest.java
@@ -30,9 +30,19 @@ public class SelectStatementTest {
 
 //        创建test表2并插入数据
         global.execute("create table test2 (k int, j int, s2 string)", session);
-        global.execute("insert into test values (+-+-1,2, ''),  (+-+-1,3, ''), (null,4, \"hello\\t world\")", session);
+        global.execute("insert into test2 values (+-+-1,2, ''),  (+-+-1,3, ''), (null,4, \"hello\\t world\")", session);
 
-        result = global.execute("select testjointest2.i,s,testjointest2.s2 from test join test2 on test.j = test2.j ", session);
-//        System.out.println(result);
+        result = global.execute("select `test join test2`.i as new_col1,s,`test join test2`.s2 from test join test2 on test.j = test2.j ", session);
+        assertEquals(new Table("RESULT",
+                new Column[] {
+                        new Column("NEW_COL1", Type.INT),
+                        new Column("S", Type.STRING),
+                        new Column("S2", Type.STRING)
+                },
+                new Object[][] {
+                        new Object[] {1, "", ""},
+                        new Object[] {null, "HELLO\t WORLD", ""}
+                }
+        ), result);
     }
 }
