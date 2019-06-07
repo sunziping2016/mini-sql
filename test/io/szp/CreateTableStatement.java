9a0d34b01bb49fe2ebebc40cc18012eb1d789bb0
Liuxiang15 <thss15_liux@163.com>
2019-06-07 15:10:41 +0800

    创建表的测试文件

--- test/io/szp/CreateTableStatement.java
+++ test/io/szp/CreateTableStatement.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CreateTableStatement {
     private Global global;
@@ -15,29 +16,52 @@ public class CreateTableStatement {
     public void init() throws SQLException {
         global = new Global(Config.root);
         session = new Session();
-
         global.removeAllDatabases();
     }
 
     // 插入表之前没有选择数据库
     @Test
     public void createTableDatabaseNotSelected() throws SQLException {
+        Exception e = assertThrows(SQLException.class, () -> {
+            global.execute("create table test (i int)", session);
+        });
+        assertEquals("No database selected", e.getMessage());
     }
 
-    // 插入两张重名的表
+    // 创建两张重名的表
     @Test
     public void createTableDuplicatedTableName() throws SQLException {
+        global.execute("create database test; use test", session);
+        global.execute("create table test (i int)", session);
+        Exception e = assertThrows(SQLException.class, () -> {
+            global.execute("create table test (i int)", session);
+        });
+        assertEquals("Table already exists", e.getMessage());
     }
 
     // 插入表的列名重复
     @Test
     public void createTableDuplicatedColumnName() throws SQLException {
+        global.execute("create database test; use test", session);
+        Exception e = assertThrows(SQLException.class, () -> {
+            global.execute("create table test (i int, i int)", session);
+        });
+        assertEquals("Duplicated column name", e.getMessage());
     }
 
     // 插入表的Not Null限制
     @Test
     public void createTableNotNullConstraint() throws SQLException {
         // 实现请参考下面的Primary Key，注意not null只有下面两种声明方式的第一种
+        global.execute("create database test; use test", session);
+        global.execute("create table test (i int not null, j int)", session);
+        Table copy = new Global(Config.root).getDatabase("TEST").getTable("TEST");
+        assertEquals(new Table("TEST",
+                new Column[] {
+                        new Column("I", Type.INT, true, false),
+                        new Column("J", Type.INT, false, false)
+                }
+        ), copy);
     }
 
     // 插入表的Primary Key限制
